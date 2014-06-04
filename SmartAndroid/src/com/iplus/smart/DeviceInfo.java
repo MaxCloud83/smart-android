9e0fd2a088d0ce5c3150eaf3b3963b76b6f20851
lei-guo <lei.guo@enish.jp>
2014-06-04 10:58:26 +0900

    device information feature

--- SmartAndroid/src/com/iplus/smart/DeviceInfo.java
+++ SmartAndroid/src/com/iplus/smart/DeviceInfo.java
@@ -1,52 +1,55 @@
+
 package com.iplus.smart;
 
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class DeviceInfo extends Activity {
 
-	private ListView listView = null;
+    private ListView deviceInfoListView = null;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.device_info);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.device_info);
 
-		// fetch listview object
-		listView = (ListView) findViewById(R.id.DeviceInfoListView);
+        // fetch listview object
+        this.deviceInfoListView = (ListView) findViewById(R.id.DeviceInfoListView);
 
-		// create ArrayAdapter for listView
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1);
-		arrayAdapter.add("BOARD:" + Build.BOARD);
-		arrayAdapter.add("BOOTLOADER:" + Build.BOOTLOADER); // Android 1.6未対応
-		arrayAdapter.add("BRAND:" + Build.BRAND);
-		arrayAdapter.add("CPU_ABI:" + Build.CPU_ABI);
-		arrayAdapter.add("CPU_ABI2:" + Build.CPU_ABI2); // Android 1.6未対応
-		arrayAdapter.add("DEVICE:" + Build.DEVICE);
-		arrayAdapter.add("DISPLAY:" + Build.DISPLAY);
-		arrayAdapter.add("FINGERPRINT:" + Build.FINGERPRINT);
-		arrayAdapter.add("HARDWARE:" + Build.HARDWARE); // Android 1.6未対応
-		arrayAdapter.add("HOST:" + Build.HOST);
-		arrayAdapter.add("ID:" + Build.ID);
-		arrayAdapter.add("MANUFACTURER:" + Build.MANUFACTURER);
-		arrayAdapter.add("MODEL:" + Build.MODEL);
-		arrayAdapter.add("PRODUCT:" + Build.PRODUCT);
-		arrayAdapter.add("TAGS:" + Build.TAGS);
-		arrayAdapter.add("TIME:" + Build.TIME);
-		arrayAdapter.add("TYPE:" + Build.TYPE);
-		arrayAdapter.add("UNKNOWN:" + Build.UNKNOWN); // Android 1.6未対応
-		arrayAdapter.add("USER:" + Build.USER);
-		arrayAdapter.add("VERSION.CODENAME:" + Build.VERSION.CODENAME);
-		arrayAdapter.add("VERSION.INCREMENTAL:" + Build.VERSION.INCREMENTAL);
-		arrayAdapter.add("VERSION.RELEASE:" + Build.VERSION.RELEASE);
-		arrayAdapter.add("VERSION.SDK_INT:" + Build.VERSION.SDK_INT);
+        // create ArrayAdapter for listView
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1);
 
-		listView.setAdapter(arrayAdapter);
-	}
+        arrayAdapter.add("BOARD:" + Build.BOARD);
+        arrayAdapter.add("BOOTLOADER:" + Build.BOOTLOADER); // Android 1.6未対応
+        arrayAdapter.add("BRAND:" + Build.BRAND);
+        arrayAdapter.add("CPU_ABI:" + Build.CPU_ABI);
+        arrayAdapter.add("CPU_ABI2:" + Build.CPU_ABI2); // Android 1.6未対応
+        arrayAdapter.add("DEVICE:" + Build.DEVICE);
+        arrayAdapter.add("DISPLAY:" + Build.DISPLAY);
+        arrayAdapter.add("FINGERPRINT:" + Build.FINGERPRINT);
+        arrayAdapter.add("HARDWARE:" + Build.HARDWARE); // Android 1.6未対応
+        arrayAdapter.add("HOST:" + Build.HOST);
+        arrayAdapter.add("ID:" + Build.ID);
+        arrayAdapter.add("MANUFACTURER:" + Build.MANUFACTURER);
+        arrayAdapter.add("MODEL:" + Build.MODEL);
+        arrayAdapter.add("PRODUCT:" + Build.PRODUCT);
+        arrayAdapter.add("TAGS:" + Build.TAGS);
+        arrayAdapter.add("TIME:" + Build.TIME);
+        arrayAdapter.add("TYPE:" + Build.TYPE);
+        arrayAdapter.add("UNKNOWN:" + Build.UNKNOWN); // Android 1.6未対応
+        arrayAdapter.add("USER:" + Build.USER);
+        arrayAdapter.add("VERSION.CODENAME:" + Build.VERSION.CODENAME);
+        arrayAdapter.add("VERSION.INCREMENTAL:" + Build.VERSION.INCREMENTAL);
+        arrayAdapter.add("VERSION.RELEASE:" + Build.VERSION.RELEASE);
+        arrayAdapter.add("VERSION.SDK_INT:" + Build.VERSION.SDK_INT);
+
+        this.deviceInfoListView.setAdapter(arrayAdapter);
+        this.deviceInfoListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        this.deviceInfoListView.setScrollingCacheEnabled(false);
+    }
 
 }
--- SmartAndroid/src/com/iplus/smart/FeatureList.java
+++ SmartAndroid/src/com/iplus/smart/FeatureList.java
@@ -1,6 +1,8 @@
+
 package com.iplus.smart;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,45 +13,49 @@ import android.widget.ListView;
 
 public class FeatureList extends Activity {
 
-	private static String TAG = "FeatureListActivity";
-	private ListView featureListView = null;
+    private static String TAG = "FeatureListActivity";
+    private ListView featureListView = null;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.feature_list);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feature_list);
 
-		this.featureListView = (ListView) findViewById(R.id.FeatureListView);
+        this.featureListView = (ListView) findViewById(R.id.FeatureListView);
 
-		// Create an ArrayAdapter that will contain all list items
-		String[] featuresArray = getResources().getStringArray(
-				R.array.featureList);
+        // Create an ArrayAdapter that will contain all list items
+        String[] featuresArray = getResources().getStringArray(
+                R.array.featureList);
 
-		/*
-		 * Assign the name array to that adapter and also choose a simple layout
-		 * for the list items
-		 */
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, featuresArray);
+        /*
+         * Assign the name array to that adapter and also choose a simple layout
+         * for the list items
+         */
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, featuresArray);
 
-		// Assign the adapter to featureListView
-		this.featureListView.setAdapter(arrayAdapter);
-		this.featureListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-		this.featureListView.setScrollingCacheEnabled(false);
+        // Assign the adapter to featureListView
+        this.featureListView.setAdapter(arrayAdapter);
+        this.featureListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        this.featureListView.setScrollingCacheEnabled(false);
 
-		// handle the item click event int featureListView
-		this.featureListView.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				Log.v(TAG, "position:" + String.valueOf(position) + "  "
-						+ "id:" + String.valueOf(id));
-				switch (position) {
-				case 0:
-
-					break;
-				}
-			}
-		});
-	}
+        // handle the item click event int featureListView
+        this.featureListView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                Log.v(TAG, "position:" + String.valueOf(position) + "  "
+                        + "id:" + String.valueOf(id));
+                Intent intent = null;
+                switch (position) {
+                    case 0:
+                        intent = new Intent(FeatureList.this, DeviceInfo.class);
+                        break;
+                }
+                if (intent != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+    }
 }
