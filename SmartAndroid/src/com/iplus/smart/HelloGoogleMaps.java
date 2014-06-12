ba5afdb32f34a2a396a722629b87e2e3db30445f
mountainKaku <mountain.kaku@gmail.com>
2014-06-12 12:17:34 +0900

    Google Maps V2...

---
+++ SmartAndroid/src/com/iplus/smart/HelloGoogleMaps.java
@@ -0,0 +1,14 @@
+
+package com.iplus.smart;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class HelloGoogleMaps extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_google_maps);
+    }
+}
