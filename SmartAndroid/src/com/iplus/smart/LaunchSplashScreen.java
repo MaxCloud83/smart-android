1e94af869cb018fe6651801ed86e82df36b66236
mountainKaku <mountain.kaku@gmail.com>
2014-06-12 18:01:05 +0900

    start to use AdMob for a sample

--- SmartAndroid/src/com/iplus/smart/LaunchSplashScreen.java
+++ SmartAndroid/src/com/iplus/smart/LaunchSplashScreen.java
@@ -1,6 +1,13 @@
 
 package com.iplus.smart;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +15,16 @@ import android.os.Handler;
 
 public class LaunchSplashScreen extends Activity {
     private final int SPLASH_DISPLAY_LENGHT = 2000;
+
+    // How long in milliseconds to wait for the interstitial to load.
+    private static final int WAIT_TIME = 4000;
+
+    // Your interstitial ad unit ID.
+    private static final String AD_UNIT_ID = "ca-app-pub-4243059076464497/5461461363";
+
+    private InterstitialAd interstitial;
+    private Timer waitTimer;
+    private boolean interstitialCanceled = false;
 
     /** Called when the activity is first created. */
     @Override
@@ -22,12 +39,100 @@ public class LaunchSplashScreen extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                /* Create an Intent that will start the FeatureList-Activity. */
-                Intent intent = new Intent(LaunchSplashScreen.this, FeatureList.class);
-                LaunchSplashScreen.this.startActivity(intent);
-                LaunchSplashScreen.this.finish();
+                showSplashInterstitial();
+                // startMainActivity();
             }
         }, SPLASH_DISPLAY_LENGHT);
+    }
+
+    private void showSplashInterstitial() {
+        interstitial = new InterstitialAd(this);
+        interstitial.setAdUnitId(AD_UNIT_ID);
+        interstitial.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // If the interstitial was canceled due to a timeout or an app
+                // being sent to the background,
+                // don't show the interstitial.
+                if (!interstitialCanceled) {
+                    waitTimer.cancel();
+                    interstitial.show();
+                }
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // The interstitial failed to load. Start the application.
+                startMainActivity();
+            }
+        });
+        interstitial.loadAd(new AdRequest.Builder().build());
+
+        waitTimer = new Timer();
+        waitTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                interstitialCanceled = true;
+                LaunchSplashScreen.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // The interstitial didn't load in a reasonable amount
+                        // of time. Stop waiting for the
+                        // interstitial, and start the application.
+                        startMainActivity();
+                    }
+                });
+            }
+        }, WAIT_TIME);
+    }
+
+    @Override
+    public void onPause() {
+        // Flip the interstitialCanceled flag so that when the user comes back
+        // they aren't stuck inside
+        // the splash screen activity.
+        if (waitTimer != null) {
+            waitTimer.cancel();
+        }
+        interstitialCanceled = true;
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (interstitial == null) {
+            return;
+        }
+        if (interstitial.isLoaded()) {
+            // The interstitial finished loading while the app was in the
+            // background. It's up to you what
+            // the behavior should be when they return. In this example, we show
+            // the interstitial since
+            // it's ready.
+            interstitial.show();
+        } else if (interstitialCanceled) {
+            // There are two ways the user could get here:
+            //
+            // 1. After dismissing the interstitial
+            // 2. Pressing home and returning after the interstitial finished
+            // loading.
+            //
+            // In either case, it's awkward to leave them in the splash screen
+            // activity, so just start the
+            // application.
+            startMainActivity();
+        }
+    }
+
+    /**
+     * Starts the application's {@link MainActivity}.
+     */
+    private void startMainActivity() {
+        /* Create an Intent that will start the FeatureList-Activity. */
+        Intent intent = new Intent(LaunchSplashScreen.this, FeatureList.class);
+        this.startActivity(intent);
+        this.finish();
     }
 
 }
